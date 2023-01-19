d7b2729e319ec541eb75361e64a2a7af78164ade
Arnuld Ignacio <arnuldxd@gmail.com>
2023-01-19 19:15:15 +0800

    added comments to content.js

--- Extensions/BitX/content.js
+++ Extensions/BitX/content.js
@@ -1,6 +1,7 @@
 let reference = document.getElementById("free_play_result");
 let banners = document.getElementById("bitx");
 let bottom = document.getElementById("bottom_user_ads_container");
+// add the iframe
 reference.insertAdjacentHTML(
   "afterend",
   '<div id="bitx" class="responsive-iframe-container"><iframe src="https://www.arnuld.net" scrolling="no" frameborder="0" allowfullscreen></iframe></div>'
@@ -8,6 +9,7 @@ reference.insertAdjacentHTML(
 setInterval(function () {
   let btn = document.getElementById("free_play_form_button");
   let btnStyle = btn.getAttribute("style");
+  // if 'style' attribute is not found, click the button
   if (!btnStyle) {
     console.log("Page is being reloaded");
     setTimeout(function () {
@@ -15,6 +17,7 @@ setInterval(function () {
       console.log("ROLL button clicked");
     }, 5000);
   } else {
+    // close the modal after free roll
     try {
       let modal = document.querySelector(".reveal-modal.open");
       if (modal) {
@@ -30,11 +33,12 @@ setInterval(function () {
     } catch (error) {}
 
     console.log("Timer is active... ");
+    // scroll to bottom of the page
     bottom.scrollIntoView({ behavior: "smooth" });
   }
 
   if (document.getElementById("bitx")) {
-    // if element exsits, do nothing
+    // if element exist, do nothing
   } else {
     // add iframe
     reference.insertAdjacentHTML(
