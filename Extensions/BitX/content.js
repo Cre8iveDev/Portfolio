469267d9f038f6357e72d46920f1aaaeee382d6f
Arnuld Ignacio <arnuldxd@gmail.com>
2023-01-19 09:23:22 +0800

    repositioned btn attribute style

--- Extensions/BitX/content.js
+++ Extensions/BitX/content.js
@@ -1,13 +1,13 @@
 let reference = document.getElementById("free_play_result");
 let banners = document.getElementById("bitx");
 let bottom = document.getElementById("bottom_user_ads_container");
-let btn = document.getElementById("free_play_form_button");
-let btnStyle = btn.getAttribute("style");
 reference.insertAdjacentHTML(
   "afterend",
   '<div id="bitx" class="responsive-iframe-container"><iframe src="https://www.arnuld.net" scrolling="no" frameborder="0" allowfullscreen></iframe></div>'
 );
 setInterval(function () {
+  let btn = document.getElementById("free_play_form_button");
+  let btnStyle = btn.getAttribute("style");
   if (!btnStyle) {
     console.log("Page is being reloaded");
     setTimeout(function () {
