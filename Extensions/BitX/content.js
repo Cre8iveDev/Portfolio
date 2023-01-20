dc17176db3f600bd35dd294fde556692b67706d7
Arnuld Ignacio <arnuldxd@gmail.com>
2023-01-20 20:41:32 +0800

    repositioned scraper function call

--- Extensions/BitX/content.js
+++ Extensions/BitX/content.js
@@ -7,7 +7,6 @@ let reference2 = document.getElementById(
 let banners = document.getElementById("bitx");
 let bottom = document.getElementById("bottom_user_ads_container");
 let bottom2 = document.getElementById("double_your_btc_main_container");
-let btn_clicked = false;
 
 // add the iframe
 reference.insertAdjacentHTML(
@@ -28,7 +27,6 @@ setInterval(function () {
     console.log("Page is being reloaded");
     setTimeout(function () {
       btn.click();
-      let btn_clicked = true;
       free_rolls++;
       console.log("ROLL button clicked");
     }, 3000);
@@ -39,6 +37,7 @@ setInterval(function () {
       if (modal) {
         let btn_close = modal.querySelector(".close-reveal-modal");
         if (btn_close) {
+          scrapeSelected();
           btn_close.click();
         } else {
           throw new Error("Close button not found");
@@ -49,7 +48,6 @@ setInterval(function () {
     } catch (error) {}
 
     console.log("Timer is active... ");
-    let btn_clicked = false;
     // scroll to bottom of the page
     bottom.scrollIntoView({ behavior: "smooth" });
     bottom2.scrollIntoView({ behavior: "smooth" });
@@ -64,15 +62,9 @@ setInterval(function () {
       '<div id="bitx" class="responsive-iframe-container"><iframe src="https://www.arnuld.net/?a=2509870" scrolling="no" frameborder="0" allowfullscreen></iframe><small><a href="https://www.arnuld.net/buy/bitcoins/">Buy Bitcoin</a> to start playing &bull; <a href="https://www.arnuld.net/dl/multiply-btc-guide">Download Guide</a> to win the Multiply BTC game</small></div>'
     );
   }
-
-  if (btn_clicked === true) {
-    console.log("Before calling scraper, btn_clicked = " + btn_clicked);
-    scrapeSelected();
-  }
 }, 30000);
 
 function scrapeSelected() {
-  console.log("btn_clicked = " + btn_clicked);
   let btc_usd_price = document.querySelector(
     "#site_stats > div > div > div h4"
   ).innerHTML;
@@ -81,7 +73,7 @@ function scrapeSelected() {
     "#bonus_eligible_msg > span > .dep_bonus_max"
   ).innerHTML;
   let rp_promo = document.querySelector(
-    "#free_play_alert_boxes > div ~ div span"
+    "#free_play_alert_boxes > div ~ div"
   ).innerHTML;
   let multiply_btc_winnings = document.querySelector(
     "#personal_stats > div > div > div h4"
