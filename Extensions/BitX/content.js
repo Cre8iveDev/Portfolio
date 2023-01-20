2d8a22b38014d0899fe1a2353e7a905659b5c7fe
Arnuld Ignacio <arnuldxd@gmail.com>
2023-01-20 19:59:36 +0800

    Added a scraper function

--- Extensions/BitX/content.js
+++ Extensions/BitX/content.js
@@ -1,11 +1,25 @@
+let starting_balance = document.querySelector("#balance").innerHTML;
+let free_rolls = 0;
 let reference = document.getElementById("free_play_result");
+let reference2 = document.getElementById(
+  "double_your_btc_main_container_outer"
+);
 let banners = document.getElementById("bitx");
 let bottom = document.getElementById("bottom_user_ads_container");
+let bottom2 = document.getElementById("double_your_btc_main_container");
+let btn_clicked = false;
+
 // add the iframe
 reference.insertAdjacentHTML(
   "afterend",
   '<div id="bitx" class="responsive-iframe-container"><iframe src="https://www.arnuld.net/?a=2509870" scrolling="no" frameborder="0" allowfullscreen></iframe><small><a href="https://www.arnuld.net/buy/bitcoins/">Buy Bitcoin</a> to start playing &bull; <a href="https://www.arnuld.net/dl/multiply-btc-guide">Download Guide</a> to win the Multiply BTC game</small></div>'
 );
+// add iframe to multiply btc page
+reference2.insertAdjacentHTML(
+  "afterend",
+  '<div id="bitx2" class="responsive-iframe-container"><iframe src="https://www.arnuld.net/?a=2509870" scrolling="no" frameborder="0" allowfullscreen></iframe><small><a href="https://www.arnuld.net/buy/bitcoins/">Buy Bitcoin</a> to start playing &bull; <a href="https://www.arnuld.net/dl/multiply-btc-guide">Download Guide</a> to win the Multiply BTC game</small></div>'
+);
+// 60-second loop starts here
 setInterval(function () {
   let btn = document.getElementById("free_play_form_button");
   let btnStyle = btn.getAttribute("style");
@@ -14,8 +28,10 @@ setInterval(function () {
     console.log("Page is being reloaded");
     setTimeout(function () {
       btn.click();
+      let btn_clicked = true;
+      free_rolls++;
       console.log("ROLL button clicked");
-    }, 5000);
+    }, 3000);
   } else {
     // close the modal after free roll
     try {
@@ -33,8 +49,10 @@ setInterval(function () {
     } catch (error) {}
 
     console.log("Timer is active... ");
+    let btn_clicked = false;
     // scroll to bottom of the page
     bottom.scrollIntoView({ behavior: "smooth" });
+    bottom2.scrollIntoView({ behavior: "smooth" });
   }
 
   if (document.getElementById("bitx")) {
@@ -46,4 +64,38 @@ setInterval(function () {
       '<div id="bitx" class="responsive-iframe-container"><iframe src="https://www.arnuld.net/?a=2509870" scrolling="no" frameborder="0" allowfullscreen></iframe><small><a href="https://www.arnuld.net/buy/bitcoins/">Buy Bitcoin</a> to start playing &bull; <a href="https://www.arnuld.net/dl/multiply-btc-guide">Download Guide</a> to win the Multiply BTC game</small></div>'
     );
   }
-}, 60000);
+
+  if (btn_clicked === true) {
+    console.log("Before calling scraper, btn_clicked = " + btn_clicked);
+    scrapeSelected();
+  }
+}, 30000);
+
+function scrapeSelected() {
+  console.log("btn_clicked = " + btn_clicked);
+  let btc_usd_price = document.querySelector(
+    "#site_stats > div > div > div h4"
+  ).innerHTML;
+  let balance_li = document.querySelector("span#balance").innerHTML;
+  let eligible_bonus = document.querySelector(
+    "#bonus_eligible_msg > span > .dep_bonus_max"
+  ).innerHTML;
+  let rp_promo = document.querySelector(
+    "#free_play_alert_boxes > div ~ div span"
+  ).innerHTML;
+  let multiply_btc_winnings = document.querySelector(
+    "#personal_stats > div > div > div h4"
+  ).innerHTML;
+
+  let jsonData = {
+    "btc-usd-price": btc_usd_price,
+    "starting-balance": starting_balance,
+    "balance-li": balance_li,
+    "eligible-bonus": eligible_bonus,
+    "multiply-btc-winnings": multiply_btc_winnings,
+    "rp-promo": rp_promo,
+    "free-rolls": free_rolls,
+  };
+
+  console.log(jsonData);
+}
