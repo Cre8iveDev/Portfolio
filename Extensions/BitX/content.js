8190c41d8ccc60d466011daff75b0f2e249f3a2a
Arnuld Ignacio <arnuldxd@gmail.com>
2023-01-23 09:26:05 +0800

    Good version before branching

--- Extensions/BitX/content.js
+++ Extensions/BitX/content.js
@@ -1,14 +1,14 @@
 // authenticate
 // get date today
 const date = new Date();
-const startDate = date.toISOString().slice(0, 10);
+const dateToday = date.toISOString().slice(0, 10);
 
 // show settings modal
 // save settings to localstorage & cookie
 
 // show before loop starts
-let starting_balance = document.querySelector("#balance").innerHTML;
-let free_rolls = 0;
+let startingBalance = document.querySelector("#balance").innerHTML;
+let freeRolls = 0;
 let reference = document.getElementById("free_play_result");
 let reference2 = document.getElementById(
   "double_your_btc_main_container_outer"
@@ -37,13 +37,8 @@ function main() {
       console.log("Page is being reloaded");
       setTimeout(function () {
         btn.click();
-        free_rolls++;
         console.log("ROLL button clicked");
         scrapeSelected();
-        saveToLocalStorage();
-        saveToCookie();
-        getLocalStorage();
-        getCookie();
       }, 3000);
     } else {
       // close the modal after free roll
@@ -80,74 +75,32 @@ function main() {
 }
 
 function scrapeSelected() {
-  let btc_usd_price = document.querySelector(
+  let btcUSDPrice = document.querySelector(
     "#site_stats > div > div > div h4"
   ).innerHTML;
-  let balance_li = document.querySelector("span#balance").innerHTML;
-  let eligible_bonus = document.querySelector(
+  let currentBalance = document.querySelector("span#balance").innerHTML;
+  let eligibleBonus = document.querySelector(
     "#bonus_eligible_msg > span > .dep_bonus_max"
   ).innerHTML;
-  let rp_promo_text = document.querySelector(
+  let rpPromo = document.querySelector(
     "#free_play_alert_boxes > div ~ div"
   ).innerHTML;
-  let rp_promo = document.querySelector(
-    "#free_play_alert_boxes > div ~ div"
-  ).innerHTML;
-  let multiply_btc_winnings = document.querySelector(
+  let multiplyBTCWinnings = document.querySelector(
     "#personal_stats > div > div > div h4"
   ).innerHTML;
 
   let jsonData = {
-    "start-date": startDate,
-    "btc-usd-price": btc_usd_price,
-    "starting-balance": starting_balance,
-    "btc-balance": balance_li,
-    "eligible-bonus": eligible_bonus,
-    "multiply-btc-winnings": multiply_btc_winnings,
-    "rp-promo": rp_promo,
-    "free-rolls": free_rolls,
+    "date-today": dateToday,
+    "btc-usd-price": btcUSDPrice,
+    "starting-balance": startingBalance,
+    "current-balance": currentBalance,
+    "eligible-bonus": eligibleBonus,
+    "multiply-btc-winnings": multiplyBTCWinnings,
+    "rp-promo": rpPromo,
+    "free-rolls": freeRolls,
   };
 
   console.log(jsonData);
-}
-
-function saveToLocalStorage(jsonData) {
-  // remove existing local storage item if it exists
-  if (localStorage.getItem("freeRollData") !== null) {
-    localStorage.removeItem("freeRollData");
-    console.log("freeRollData was removed from Local Storage");
-  } else {
-    // add a new local storage with same key
-    localStorage.setItem("freeRollData", JSON.stringify(jsonData));
-    console.log("freeRollData saved to Local Storage");
-  }
-}
-
-function saveToCookie(jsonData) {
-  let expires = date.setDate(date.getDate() + 1);
-  let jsonString = JSON.stringify(jsonData);
-  document.cookie =
-    "freeRollData=" + jsonString + "; expires=" + expires + "; path=/";
-  console.log("freeRollData saved to Cookie");
-}
-
-function getLocalStorage() {
-  if (localStorage.getItem("freeRollData") !== null) {
-    let jsonData = localStorage.getItem("freeRollData");
-    console.log(jsonData);
-    console.log("freeRollData retrieved from Local Storage");
-  } else {
-    console.log("freeRollData in Local Storage does not exist!");
-  }
-}
-
-function getCookie() {
-  const freeRollCookie = new URLSearchParams(
-    document.cookie.replaceAll("&", "%26").replaceAll("; ", "&")
-  );
-  console.log(
-    "freeRollData in a Cookie: " + freeRollCookie.get("freeRollData")
-  );
 }
 
 main();
